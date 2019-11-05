df93a636fc9ae5ca8b8bbfd80a77290b48767a5f
AverageOddity <57405506+AverageOddity@users.noreply.github.com>
2019-11-05 09:09:25 -0600

    Add files via upload

---
+++ 10-29 Cats.py
@@ -0,0 +1,14 @@
+cats = ["Senator Snuggles", "Doctor Horatio", "The Toe Monger"]
+adjective1 = input("Enter an -est adjective to describe Senator Snuggles: ")
+adjective2 = input("Enter another adjective to describe Senator Snuggles: ")
+adjective3 = input("Enter an -est adjective to describe Doctor Horatio: ")
+adjective4 = input("Enter another adjective to describe Doctor Horatio: ")
+adjective5 = input("Enter an -est adjective to describe The Toe Monger: ")
+adjective6 = input("Enter another adjective to describe The Toe Monger: ")
+
+print("My " + adjective1 + " cat is named " + cats[0] + ".")
+print("He is very " + adjective2 + ".")
+print("My " + adjective3 + " cat is named " + cats[1] + ".")
+print("He is very " + adjective4 + ".")
+print("My " + adjective5 + " cat is named " + cats[2] + ".")
+print("He is very " + adjective6 + ".")
---
+++ 10-29 Introduction.py
@@ -0,0 +1,9 @@
+character_name = "Daniel"
+character_height = "5ft"
+character_age = "21 years old"
+phrase = "I am very small."
+is_man = True
+
+print("My name is " + character_name + ".")
+print("I am " + character_age + " and " + character_height + " tall.")
+print(phrase)
